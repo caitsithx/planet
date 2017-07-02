12146dde0060b421d67ed0daf92997ae1694bd84
Xiaoliang Li <xiaoliangl@vmware.com>
2017-07-02 19:02:40 +0800

    refactor resize images

--- planet2/settings.py
+++ planet2/settings.py
@@ -1,7 +1,9 @@
 
+DL_DATA_DIR = '/home/caitsithx/deeplearning_workspace/dl_data/amz-from-space'
+
 # This directory is created automatically, normally you do not need to change this path
-DATA_DIR = '/home/chicm/data/planet'
+DATA_DIR = '/home/caitsithx/deeplearning_workspace/data/amz-from-space'
 
 # This directory is create automatically, you do not need to change it.
-MODEL_DIR = DATA_DIR + '/models/test'
+MODEL_DIR = DL_DATA_DIR + '/models'
 
